48a4b8132b712bd5edc196171530ec8688d389b1
dellrkc <kideash@gmail.com>
2026-04-16 08:37:42 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+self.addEventListener("install", e => {
+  console.log("Service Worker installed");
+});
+
+self.addEventListener("fetch", e => {
+  // basic offline support (optional upgrade later)
+});
+
